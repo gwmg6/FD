4403073d7596af8d89f29957f3763b95e64e1629
Gwming <139041480+gwmg6@users.noreply.github.com>
2023-07-14 20:34:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export 默认 {
   async fetch(request， env) {
     let url = new URL(request.url);
     if (url.pathname。startsWith('/')) {
-      url.hostname = 'zsd223-8787.csb.app'
+      url.hostname = 'kcqgrn-8787.csb.app'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
